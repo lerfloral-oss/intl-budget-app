e7b323f1a1b01d368cbdc1a1c3defebe95d2e5f1
lerfloral-oss <lerfloral@gmail.com>
2025-11-24 19:30:28 +0800

    Implement service worker for offline caching

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'intl-edu-budget-cache-v1';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
